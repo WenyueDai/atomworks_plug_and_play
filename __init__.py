9f7e8fded926598f75d7e314c38ea67b9c644b39
WenyueDai <academiadaiwenyue@gmail.com>
2026-02-02 20:56:52 +0000

    rebase

--- __init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = "0.4.1"
-
-from .run import build_metadata
-
-__all__ = ["__version__", "build_metadata"]
--- tables/__init__.py
+++
@@ -1,2 +0,0 @@
-from .wide import build_all_metadata_wide
-__all__ = ["build_all_metadata_wide"]
